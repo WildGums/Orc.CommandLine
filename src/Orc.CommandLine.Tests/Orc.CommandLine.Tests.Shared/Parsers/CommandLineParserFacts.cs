fecfaf4153b6a97be9a76f05542e28e57c914d6c
Geert van Horrik <geert@catenalogic.com>
2016-02-05 18:07:57 +0100

    Solution maintenance

--- src/Orc.CommandLine.Tests/Orc.CommandLine.Tests.Shared/Parsers/CommandLineParserFacts.cs
+++ src/Orc.CommandLine.Tests/Orc.CommandLine.Tests.Shared/Parsers/CommandLineParserFacts.cs
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------------------------------------------------
-// <copyright file="CommandLineParserFacts.cs" company="Wild Gums">
-//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// <copyright file="CommandLineParserFacts.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
--- src/Orc.CommandLine/Orc.CommandLine.Shared/Context/Interfaces/IContext.cs
+++ src/Orc.CommandLine/Orc.CommandLine.Shared/Context/Interfaces/IContext.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IContext.cs" company="Wild Gums">
-//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// <copyright file="IContext.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
