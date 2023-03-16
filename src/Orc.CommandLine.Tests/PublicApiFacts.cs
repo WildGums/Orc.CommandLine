fd376b8cf66933e63e2a512f7352177956cf02e8
Vladimir <work.lvg@gmail.com>
2023-03-16 17:05:13 +0300

    fix tests

--- src/Orc.CommandLine.Tests/PublicApiFacts.cs
+++ src/Orc.CommandLine.Tests/PublicApiFacts.cs
@@ -1,37 +1,29 @@
-namespace Orc.CommandLine.Tests
-{
-    using System.Reflection;
-    using System.Runtime.CompilerServices;
-    using System.Threading.Tasks;
-    using NUnit.Framework;
-    using PublicApiGenerator;
-    using VerifyNUnit;
+﻿namespace Orc.CommandLine.Tests;
 
-        PublicApiApprover.ApprovePublicApi(assembly);
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PublicApiGenerator;
+using VerifyNUnit;
+
+[TestFixture]
+public class PublicApiFacts
+{
+    [Test, MethodImpl(MethodImplOptions.NoInlining)]
+    public async Task Orc_CommandLine_HasNoBreakingChanges_Async()
+    {
+        var assembly = typeof(CommandLineParser).Assembly;
+
+        await PublicApiApprover.ApprovePublicApiAsync(assembly);
     }
 
     internal static class PublicApiApprover
     {
-        [Test, MethodImpl(MethodImplOptions.NoInlining)]
-        public async Task Orc_CommandLine_HasNoBreakingChanges_Async()
+        public static async Task ApprovePublicApiAsync(Assembly assembly)
         {
-            var assembly = typeof(CommandLineParser).Assembly;
-
-            await PublicApiApprover.ApprovePublicApiAsync(assembly);
-        }
-    }
-
-    internal class AssemblyPathNamer : UnitTestFrameworkNamer
-    {
-        private readonly string _name;
-
-        public AssemblyPathNamer(string assemblyPath)
-        {
-            public static async Task ApprovePublicApiAsync(Assembly assembly)
-            {
-                var publicApi = ApiGenerator.GeneratePublicApi(assembly, new ApiGeneratorOptions());
-                await Verifier.Verify(publicApi);
-            }
+            var publicApi = ApiGenerator.GeneratePublicApi(assembly, new ApiGeneratorOptions());
+            await Verifier.Verify(publicApi);
         }
     }
 }
